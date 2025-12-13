3334996886351ae3cdcd398b4c8dca873c1ef7a3
Azharelly <azharelly140195@hotmail.com>
2025-12-13 12:09:41 +0000

    feat: load DB credencials from Secret Manager in startup script

--- startup.sh
+++ startup.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
 
+# Update system
 apt-get update
-apt-get install -y docker.io
+apt-get install -y docker.io jq
 
-systemctl start docker
-systemctl enable docker
+# Get secrets from Secret Manager
+DB_HOST=$(gcloud secrets versions access latest --secret=DB_HOST)
+DB_NAME=$(gcloud secrets versions access latest --secret=DB_NAME)
+DB_USER=$(gcloud secrets versions access latest --secret=DB_USER)
+DB_PASSWORD=$(gcloud secrets versions access latest --secret=DB_PASSWORD)
 
-gcloud auth configure-docker europe-west1-docker.pkg.dev --quiet
+# Pull image from Artifact Registry
+docker pull europe-west1-docker.pkg.dev/radiation-monitoring-system-ca/radiation-docker-repo/radiation-monitoring-app:latest
 
+# Run container with env variables
 docker run -d -p 8080:8080 \
+  -e DB_HOST="$DB_HOST" \
+  -e DB_NAME="$DB_NAME" \
+  -e DB_USER="$DB_USER" \
+  -e DB_PASSWORD="$DB_PASSWORD" \
   europe-west1-docker.pkg.dev/radiation-monitoring-system-ca/radiation-docker-repo/radiation-monitoring-app:latest
+

8aa1f6f1b745f00d575c86a6fa4c272c008ee688
Azharelly <azharelly140195@hotmail.com>
2025-12-16 03:09:41 +0000

    feat: add history view to display radiation logs

--- startup.sh
+++ startup.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 set -e
 
-# Update system
 apt-get update
 apt-get install -y docker.io jq
 
 systemctl start docker
-systemctl enable docker 
+systemctl enable docker
 
 gcloud auth configure-docker europe-west1-docker.pkg.dev -q
 
-# Get secrets from Secret Manager
-export DB_HOST=$(gcloud secrets versions access latest --secret=DB_HOST)
-export DB_NAME=$(gcloud secrets versions access latest --secret=DB_NAME)
-export DB_USER=$(gcloud secrets versions access latest --secret=DB_USER)
-export DB_PASSWORD=$(gcloud secrets versions access latest --secret=DB_PASSWORD)
+# Start Cloud SQL Auth Proxy if not already running
+if ! pgrep -f cloud-sql-proxy > /dev/null; then
+  /usr/local/bin/cloud-sql-proxy radiation-monitoring-system-ca:europe-west1:radiation-mysql --port 3306 &
+fi
 
-# Pull image from Artifact Registry
+sleep 5
+
+DB_NAME=$(gcloud secrets versions access latest --secret=DB_NAME)
+DB_USER=$(gcloud secrets versions access latest --secret=DB_USER)
+DB_PASSWORD=$(gcloud secrets versions access latest --secret=DB_PASSWORD)
+
+docker rm -f radiation-api || true
+
 docker pull europe-west1-docker.pkg.dev/radiation-monitoring-system-ca/radiation-docker-repo/radiation-monitoring-app:latest
 
-# Run container with env variables
-docker run -d -p 8080:8080 \
-  -e DB_HOST="$DB_HOST" \
+docker run -d -p 8080:8080\
+  --name radiation-api \
+  -e DB_HOST=127.0.0.1 \
   -e DB_NAME="$DB_NAME" \
   -e DB_USER="$DB_USER" \
   -e DB_PASSWORD="$DB_PASSWORD" \
   europe-west1-docker.pkg.dev/radiation-monitoring-system-ca/radiation-docker-repo/radiation-monitoring-app:latest
-
